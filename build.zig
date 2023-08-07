0f638db651f073058e0575f215d03868ac8ddb6c
Antonio Lanciotti <a.lanciotti@westernengineering.it>
2023-08-07 16:15:19 +0200

    fix build for zig 0.11.0

--- build.zig
+++ build.zig
@@ -1,24 +1,20 @@
 const std = @import("std");
 
-pub fn build(b: *std.build.Builder) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
+pub fn build(b: *std.Build) void {
+    const optimize = b.standardOptimizeOption(.{});
     const target = b.standardTargetOptions(.{});
 
-    // Standard release options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
-    const mode = b.standardReleaseOptions();
+    const exe = b.addExecutable(
+        .{
+            .name = "zig-calc",
+            .root_source_file = .{ .path = "src/main.zig" },
+            .optimize = optimize,
+            .target = target,
+        },
+    );
+    b.installArtifact(exe);
 
-    const exe = b.addExecutable("zig-calc", "src/main.zig");
-    exe.setTarget(target);
-    exe.setBuildMode(mode);
-    exe.single_threaded = true;
-    exe.strip = true;
-    exe.install();
-
-    const run_cmd = exe.run();
+    const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
     if (b.args) |args| {
         run_cmd.addArgs(args);
@@ -27,10 +23,12 @@ pub fn build(b: *std.build.Builder) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    const exe_tests = b.addTest("src/main.zig");
-    exe_tests.setTarget(target);
-    exe_tests.setBuildMode(mode);
-
+    const exe_tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    const run_unit_tests = b.addRunArtifact(exe_tests);
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&exe_tests.step);
+    test_step.dependOn(&run_unit_tests.step);
 }
